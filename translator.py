0a496869d2470f02d596b6bf6f2f626206e0b209
Handan <98670479+han-dann@users.noreply.github.com>
2023-06-10 09:10:30 -0400

    Add files via upload
    new file uplaoded

---
+++ translator.py
@@ -0,0 +1,20 @@
+
+# This is a translator for the Giraffe Language
+# that converts vowels to "g".
+
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation = translation + "G"
+            else:
+                translation = translation + "g"
+        else:
+            translation = translation + letter
+    return translation
+
+
+print(translate(input("Enter a phrase: ")))
+
+
